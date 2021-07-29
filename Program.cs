a19d32110d94cb578f598b8b201b7035b4388080
Madhukar <madhurps@gmail.com>
2021-07-29 10:47:14 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Jenkins");
+        }
+    }
+}
